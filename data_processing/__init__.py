f5e22955cd22b920c391e279fec411eae1fb9bcc
Fantasy AI Studio <alan.huang67@gmail.com>
2025-02-10 13:52:05 +0800

    Delete data_processing directory

--- data_processing/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-# 该模块导入子模块的解析功能，方便在 main.py 中统一调用
-from .character_parser import parse_character_info
-from .story_parser import parse_story_overview
-from .scene_parser import parse_scene_design
-from .shot_parser import parse_shot_script
